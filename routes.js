d5a56cc999a5431f9e6f86de6f6693191ce1313f
kfreez <kfreec@gmail.com>
2015-10-29 13:12:31 -0400

    add bower.json to use angular

--- routes.js
+++ routes.js
@@ -1,5 +1,10 @@
 'use strict';
 
+
 exports = module.exports = function(app) {
-    app.get('/', require('./controller/index').init);
+    app.get('/', function(req, res)
+        {
+            res.render('index', {title: 'Shelves'});
+        }
+    );
 };
